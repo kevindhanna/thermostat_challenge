864079420f3e5cca64d7ad147430eac0b1bbf955
Heli Sivunen <heli@mooncat.tv>
2019-10-17 16:57:30 +0100

    Not quite ready

--- public/src/WeatherModel.js
+++ public/src/WeatherModel.js
@@ -4,10 +4,14 @@ function WeatherModel(url, key) {
 WeatherModel.prototype = {
   _get: function(city, $) {
     console.log($)
-    return $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f304d45f3b62616958643460868fcac`)
+    return $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f304d45f3b62616958643460868fcac`, function (  resp ){return resp})
   },
   getWeather: function(city, jquery) {
     var jquery = jquery || $
-    return this._get(city, jquery)
+    var weatherparams = this._get(city, jquery)
+    var returnArray = []
+    returnArray.push(weatherparams.responseJSON.main.temp_min)
+    returnArray.push(weatherparams.responseJSON.main.temp_max)
+    return returnArray
   }
 }
